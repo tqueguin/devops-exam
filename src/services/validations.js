465d6f653845616a99f8320a3051b90acf67dcc3
tqueguin <72442138+tqueguin@users.noreply.github.com>
2023-01-20 10:41:03 +0100

    style(app): fixing style

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,7 +1,11 @@
 const isValid = (gamertag) => {
     const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
     const numbers = /[0-9]/;
-    return specialChars.test(gamertag) && numbers.test(gamertag) && gamertag.length >= 8;
+    return (
+        specialChars.test(gamertag) &&
+        numbers.test(gamertag) &&
+        gamertag.length >= 8
+    );
 };
 
 const isEmpty = (label) => !label || label.length === 0;
